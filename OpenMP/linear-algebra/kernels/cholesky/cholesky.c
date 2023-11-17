ee1b51374f74d1fca9c764104ea41c02374525a2
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2023-11-17 22:45:27 +0100

    Ottimizzaione migliorata con l'aggiunta di un task per 1 dei due for innestati dentro la parallel region. Motivazione, i due for sono indipendenti, l'unica sezione critica è la riga 72, dove c'è da aspettare per p[i] che sia calcolato dal primo blocco di for = 0.000762

--- OpenMP/linear-algebra/kernels/cholesky/cholesky.c
+++ OpenMP/linear-algebra/kernels/cholesky/cholesky.c
@@ -48,23 +48,28 @@ static void kernel_cholesky(int n,
                             DATA_TYPE POLYBENCH_1D(p, N, n),
                             DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
 {
-  int i, j, k;
+  int i, j, k, l;
 
-  DATA_TYPE x;
+  DATA_TYPE x, y;
 
   #pragma omp parallel for
   for (i = 0; i < _PB_N; ++i)
   {
-    x = A[i][i];
-    for (j = 0; j <= i - 1; ++j)
-      x = x - A[i][j] * A[i][j];
-    p[i] = 1.0 / sqrt(x);
-    for (j = i + 1; j < _PB_N; ++j)
+    #pragma omp task
     {
-      x = A[i][j];
+      x = A[i][i];
+      for (j = 0; j <= i - 1; ++j)
+        x = x - A[i][j] * A[i][j];
+      p[i] = 1.0 / sqrt(x);
+    }
+
+    for (l + i + 1; l < _PB_N; ++l)
+    {
+      y = A[i][l];
       for (k = 0; k <= i - 1; ++k)
-        x = x - A[j][k] * A[i][k];
-      A[j][i] = x * p[i];
+        y = y - A[l][k] * A[i][k];
+      #pragma omp taskwait
+      A[l][i] = y * p[i];
     }
   }
 }
