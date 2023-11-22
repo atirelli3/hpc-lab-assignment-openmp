2e697fa8ee8d7aadda4a2477958b5c9fea39e998
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2023-11-22 12:25:54 +0100

    Aggiunto nello script di benchmakr il loop per testare tutti i benchmark

--- OpenMP/linear-algebra/kernels/cholesky/benchmark/benchmark.sh
+++ OpenMP/linear-algebra/kernels/cholesky/benchmark/benchmark.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Set the path to the cholesky.h file
-cholesky_file="../cholesky.h"
+cholesky_file="cholesky.h"
 
 # Extract dataset names from the file
 dataset_names=$(awk '/#.*_DATASET/ && !/#.*!defined/ {print $3}' "$cholesky_file")
 
-# Print the dataset names
-echo "Dataset names:"
-echo "$dataset_names"
+# Iterate over dataset names and execute the command
+for dataset_name in $dataset_names; do
+    echo "Executing for dataset: $dataset_name"
+    make EXT_CFLAGS="-DPOLYBENCH_TIME -D${dataset_name} -DPARALLEL_OPT" clean all
+done
