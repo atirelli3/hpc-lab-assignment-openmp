1aa05863b2ad296f2d3597e4385e48d86e565f24
jot <singhamanjot726@gmail.com>
2023-11-24 22:08:29 +0100

    and output files
    Signed-off-by: jot <singhamanjot726@gmail.com>

--- OpenMP/linear-algebra/kernels/cholesky/cholesky.c
+++ OpenMP/linear-algebra/kernels/cholesky/cholesky.c
@@ -19,7 +19,7 @@ static void init_array(int n,
 
   for (i = 0; i < n; i++)
   {
-    p[i] = 1.0 / n;
+    p[i] = 0;
     for (j = 0; j < n; j++)
       A[i][j] = 1.0 / n;
   }
@@ -54,12 +54,15 @@ static void kernel_cholesky(int n,
   for (i = 0; i < _PB_N; ++i)
   {
     x = A[i][i];
+
     for (j = 0; j <= i - 1; ++j)
       x = x - A[i][j] * A[i][j];
     p[i] = 1.0 / sqrt(x);
+
     for (j = i + 1; j < _PB_N; ++j)
     {
       x = A[i][j];
+
       for (k = 0; k <= i - 1; ++k)
         x = x - A[j][k] * A[i][k];
       A[j][i] = x * p[i];
