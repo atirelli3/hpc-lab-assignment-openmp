92aa034cd3774118f193e0a3e33ac22b7780803a
jot <singhamanjot726@gmail.com>
2023-11-24 18:51:23 +0100

    heavy optimization
    Signed-off-by: jot <singhamanjot726@gmail.com>

--- OpenMP/linear-algebra/kernels/cholesky/cholesky.c
+++ OpenMP/linear-algebra/kernels/cholesky/cholesky.c
@@ -51,20 +51,21 @@ static void kernel_cholesky(int n,
   int i, j, k;
 
   DATA_TYPE x;
-  for (i = 0; i < _PB_N; ++i)
-  {
-    x = A[i][i];
-    for (j = 0; j <= i - 1; ++j)
-      x = x - A[i][j] * A[i][j];
-    p[i] = 1.0 / sqrt(x);
-    for (j = i + 1; j < _PB_N; ++j)
-    {
-      x = A[i][j];
-      for (k = 0; k <= i - 1; ++k)
-        x = x - A[j][k] * A[i][k];
-      A[j][i] = x * p[i];
+    for (i = 0; i < _PB_N; ++i) {
+        p[i] = 1 / sqrt(A[i][i] - p[i]);
+
+        #pragma omp parallel for schedule(dynamic) private(j, k, x)
+        for (j = i + 1; j < _PB_N; j++) {
+            x = A[i][j];
+            
+            #pragma omp simd reduction(-:x)
+            for (k = 0; k <= i - 1; ++k)
+                x = x - A[j][k] * A[i][k];
+
+            A[j][i] = x * p[i];
+            p[j] += A[j][i] * A[j][i]; 
+        }
     }
-  }
 }
 
 /* Main computational kernel optimize. The whole function will be timed,
