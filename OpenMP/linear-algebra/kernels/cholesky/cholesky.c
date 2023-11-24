ccad958beedbd3b7b48aaff11c7f28934332276f
danisystem <94066258+danisystem@users.noreply.github.com>
2023-11-24 17:22:41 +0100

    prova parallelizzazione

--- OpenMP/linear-algebra/kernels/cholesky/cholesky.c
+++ OpenMP/linear-algebra/kernels/cholesky/cholesky.c
@@ -49,14 +49,17 @@ static void kernel_cholesky(int n,
                             DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
 {
   int i, j, k;
-
   DATA_TYPE x;
+
+  #pragma omp parallel for private(x, k)
   for (i = 0; i < _PB_N; ++i)
   {
     x = A[i][i];
     for (j = 0; j <= i - 1; ++j)
       x = x - A[i][j] * A[i][j];
     p[i] = 1.0 / sqrt(x);
+
+    #pragma omp parallel for private(x)
     for (j = i + 1; j < _PB_N; ++j)
     {
       x = A[i][j];
@@ -67,33 +70,6 @@ static void kernel_cholesky(int n,
   }
 }
 
-/* Main computational kernel optimize. The whole function will be timed,
-   including the call and return. */
-static void opt_kernel_cholesky(int n,
-                            DATA_TYPE POLYBENCH_1D(p, N, n),
-                            DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
-{
-  int i, j, k;
-  DATA_TYPE x, y;
-
-  for (i = 0; i < _PB_N; ++i)
-  { 
-    x = A[i][i];
-    #pragma omp parallel for private(j) reduction(-:x)
-    for (j = 0; j <= i - 1; ++j)
-      x -= A[i][j] * A[i][j];
-    p[i] = 1.0 / sqrt(x);
-
-    #pragma omp parallel for private(j) schedule(dynamic)
-    for (j = i + 1; j < _PB_N; ++j)
-    {
-      y = A[i][j];
-      for (k = 0; k <= i - 1; ++k)
-        y = y - A[j][k] * A[i][k];
-      A[j][i] = y * p[i];
-    }
-  }
-}
 
 int main(int argc, char **argv)
 {
@@ -110,14 +86,8 @@ int main(int argc, char **argv)
   /* Start timer. */
   polybench_start_instruments;
 
-
   /* Run kernel. */
-  #ifdef PARALLEL_OPT
-    opt_kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
-  #else
-    kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
-  #endif
-
+  kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
 
   /* Stop and print timer. */
   polybench_stop_instruments;
