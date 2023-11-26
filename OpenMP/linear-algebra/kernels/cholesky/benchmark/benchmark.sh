9bd4ac4316fa5aa1f54504665ccf805516887401
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2023-11-26 22:13:16 +0100

    New optimization version, memory access focus. Fix and refactoring of the benchmark script

--- OpenMP/linear-algebra/kernels/cholesky/benchmark/benchmark.sh
+++ OpenMP/linear-algebra/kernels/cholesky/benchmark/benchmark.sh
@@ -8,30 +8,48 @@ cholesky_file="cholesky.h"
 # Extract dataset names from the file
 dataset_names=$(awk '/#.*_DATASET/ && !/#.*!defined/ {print $3}' "$cholesky_file")
 
+compile_with_dataset() {
+    make EXT_CFLAGS="$1" clean all > /dev/null 2>&1
+}
+
+compile_with_arrays() {
+    make EXT_CFLAGS="$1 -DPOLYBENCH_DUMP_ARRAYS" clean all > /dev/null 2>&1
+}
+
 # Function to run with perf stat and suppress other outputs
 run_with_perf() {
-    # make EXT_CFLAGS="$1 -DPOLYBENCH_DUMP_ARRAYS" clean all > /dev/null 2>&1
-    make EXT_CFLAGS="$1" clean all
     perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations ./cholesky_acc
 }
 
 # Iterate over dataset names and execute the command
 for dataset_name in $dataset_names; do
-    echo "### Executing for dataset: $dataset_name"
+    echo "####### Executing for dataset: $dataset_name #######"
 
     # SEQUENTIAL
-    echo "=> $dataset_name SEQUENTIAL"
-    run_with_perf "-D${dataset_name}"
-    # ./cholesky_acc > out_seq.txt 2>&1
-    ./cholesky_acc
+    echo "@@@@@ => $dataset_name SEQUENTIAL"
+    # compile_with_dataset "-D${dataset_name}"
+    compile_with_arrays "-D${dataset_name}"
+    # run_with_perf
+    ./cholesky_acc > out_seq.txt 2>&1
+
+    # MEM_OPT
+    # echo "@@@@@ => $dataset_name MEM_OPT"
+    # compile_with_dataset "-D${dataset_name} -DMEM_OPT"
+    compile_with_arrays "-D${dataset_name} -DMEM_OPT"
+    # run_with_perf
+    ./cholesky_acc > out_mem.txt 2>&1
 
     # PARALLEL_OPT
-    echo "=> $dataset_name PARALLEL_OPT"
-    run_with_perf "-D${dataset_name} -DPARALLEL_OPT"
+    # echo "@@@@@ => $dataset_name PARALLEL_OPT"
+    # complie_with_dataset "-D${dataset_name} -DPARALLEL_OPT"
+    # compile_with_arrays "-D${dataset_name} -DPARALLEL_OPT"
+    # run_with_perf
     # ./cholesky_acc > out_opt.txt 2>&1
-    ./cholesky_acc
 
     # Compare the output files
     # echo "### Diff between out_seq.txt and out_opt.txt:"
-    # diff -u -q out_seq.txt out_opt.txt
+    # echo "### Diff between out_seq.txt and out_mem.txt:"
+    # diff -q out_seq.txt out_mem.txt
+    echo "###################################################"
 done
+
--- OpenMP/linear-algebra/kernels/cholesky/cholesky.c
+++ OpenMP/linear-algebra/kernels/cholesky/cholesky.c
@@ -10,6 +10,22 @@
 /* Default data type is double, default size is 4000. */
 #include "cholesky.h"
 
+/*
+ * The MatrixVector structure represents a mathematical matrix and a vector.
+ *
+ * It contains two members:
+ * 1. p: This is an array of DATA_TYPE which represents a vector of size N.
+ * 2. A: This is an array of DATA_TYPE which represents a matrix of size N*N.
+ *
+ * This structure can be used to perform various matrix and vector operations.
+ */
+typedef struct
+{ 
+  DATA_TYPE p[N];  // Vector of size N
+  DATA_TYPE A[N * N];  // Matrix of size N*N
+} MatrixVector;
+
+
 /* Array initialization. */
 static void init_array(int n,
                        DATA_TYPE POLYBENCH_1D(p, N, n),
@@ -24,6 +40,30 @@ static void init_array(int n,
       A[i][j] = 1.0 / n;
   }
 }
+
+/*
+ * The mem_init_array function initializes the members of a MatrixVector structure.
+ *
+ * Parameters:
+ * - n: The size of the matrix and vector.
+ * - mv: A pointer to the MatrixVector structure to be initialized.
+ *
+ * This function initializes the vector 'p' and the matrix 'A' of the MatrixVector structure 
+ * with the value 1.0/n. This means that after the function call, all elements of 'p' and 'A' 
+ * will be equal to 1.0/n.
+ */
+static void mem_init_array(int n, MatrixVector* mv)
+{
+  int i, j;
+
+  for (i = 0; i < n; i++)
+  {
+    mv->p[i] = 1.0 / n;
+    for (j = 0; j < n; j++)
+      mv->A[i * n + j] = 1.0 / n;
+  }
+}
+
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
@@ -41,6 +81,31 @@ static void print_array(int n,
         fprintf(stderr, "\n");
     }
 }
+
+/*
+ * The mem_print_array function prints the matrix 'A' of a MatrixVector structure.
+ *
+ * Parameters:
+ * - n: The size of the matrix.
+ * - mv: A pointer to the MatrixVector structure whose matrix is to be printed.
+ *
+ * This function iterates over each element of the matrix 'A' and prints it to the stderr.
+ * After every 20 elements, it prints a newline character. This is done to ensure that the 
+ * output is neatly formatted and easy to read.
+ */
+static void mem_print_array(int n, MatrixVector* mv)
+{
+  int i, j;
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+    {
+      fprintf(stderr, DATA_PRINTF_MODIFIER, mv->A[i * n + j]);
+      if ((i * n + j) % 20 == 0)
+        fprintf(stderr, "\n");
+    }
+}
+
 
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
@@ -67,6 +132,47 @@ static void kernel_cholesky(int n,
   }
 }
 
+/*
+ * The mem_kernel_cholesky function performs the Cholesky decomposition on a MatrixVector structure.
+ *
+ * Parameters:
+ * - n: The size of the matrix.
+ * - mv: A pointer to the MatrixVector structure to be decomposed.
+ *
+ * This function performs the Cholesky decomposition on the matrix 'A' of the MatrixVector structure.
+ * The Cholesky decomposition is a decomposition of a Hermitian, positive-definite matrix into the 
+ * product of a lower triangular matrix and its conjugate transpose. The result of the decomposition 
+ * is stored back into the matrix 'A' and the vector 'p' of the MatrixVector structure.
+ *
+ * This function uses OpenMP directives for parallelization. The 'reduction' clause is used to 
+ * perform a reduction on the variable 'x' in a thread-safe manner. The 'private' clause is used 
+ * to specify that the variables 'x' and 'k' are private to each thread.
+ */
+static void mem_kernel_cholesky(int n, MatrixVector* mv)
+{
+  int i, j, k;
+
+  DATA_TYPE x;
+  for (i = 0; i < n; ++i)
+  {
+    x = mv->A[i * n + i];
+    #pragma omp parallel for reduction(-:x)
+    for (j = 0; j <= i - 1; ++j)
+      x = x - mv->A[i * n + j] * mv->A[i * n + j];
+    mv->p[i] = 1.0 / sqrt(x);
+    #pragma omp parallel for private(x, k)
+    for (j = i + 1; j < n; ++j)
+    {
+      x = mv->A[i * n + j];
+      #pragma omp simd reduction(-:x)
+      for (k = 0; k <= i - 1; ++k)
+        x = x - mv->A[j * n + k] * mv->A[i * n + k];
+      mv->A[j * n + i] = x * mv->p[i];
+    }
+  }
+}
+
+
 /* Main computational kernel optimize. The whole function will be timed,
    including the call and return. */
 static void opt_kernel_cholesky(int n,
@@ -79,7 +185,7 @@ static void opt_kernel_cholesky(int n,
     for (i = 0; i < _PB_N; ++i) {
         p[i] = 1 / sqrt(A[i][i] - p[i]);
 
-        #pragma omp parallel for schedule(dynamic) private(j, k, x)
+        #pragma omp parallel for private(j, k, x)
         for (j = i + 1; j < _PB_N; j++) {
             x = A[i][j];
             
@@ -102,20 +208,31 @@ int main(int argc, char **argv)
   POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
   POLYBENCH_1D_ARRAY_DECL(p, DATA_TYPE, N, n);
 
-  /* Initialize array(s). */
-  init_array(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+  #ifdef MEM_OPT
+    /* Initialize the linearization struct. */
+    mem_init_array(n, mv);
+  #else
+    /* Initialize array(s). */
+    init_array(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+  #endif
 
   /* Start timer. */
   polybench_start_instruments;
 
-
-  /* Run kernel. */
-  #ifdef PARALLEL_OPT
-    opt_kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+  #ifdef MEM_OPT
+    /* Run memory optimize kernel. */
+    mem_kernel_cholesky(n, mv);
   #else
-    kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+  {
+    #ifdef PARALLEL_OPT
+      /* Run optimize kernel. */
+      opt_kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+    #else
+      /* Run kernel. */
+      kernel_cholesky(n, POLYBENCH_ARRAY(p), POLYBENCH_ARRAY(A));
+    #endif
+  }
   #endif
-
 
   /* Stop and print timer. */
   polybench_stop_instruments;
